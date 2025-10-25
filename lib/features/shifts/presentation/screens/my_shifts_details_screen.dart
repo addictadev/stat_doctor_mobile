cf7dbda9defcc72acc96874e8deff5ec387eca5c
Abdelmonem Rabea <60942133+3bdelmonem@users.noreply.github.com>
2025-10-25 05:06:52 +0300

    Remove MyShiftsStatus enum and update MyShiftsDetailsScreen to include new bottom navigation bar with action buttons for withdrawing applications and canceling shifts, enhancing user interaction and layout.

--- lib/features/shifts/presentation/screens/my_shifts_details_screen.dart
+++ lib/features/shifts/presentation/screens/my_shifts_details_screen.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:stat_doctor/core/config/styles/styles.dart';
 import 'package:stat_doctor/core/widgets/app_appbar.dart';
-import 'package:stat_doctor/features/shifts/datat/enums/my_shifts_status.dart';
+import 'package:stat_doctor/core/widgets/app_button.dart';
+import 'package:stat_doctor/core/widgets/clickable_text.dart';
+import 'package:stat_doctor/features/shifts/data/enums/my_shifts_status.dart';
 import 'package:stat_doctor/features/shifts/presentation/widgets/my_shifts_details_contact.dart';
 import 'package:stat_doctor/features/shifts/presentation/widgets/my_shifts_details_description.dart';
 import 'package:stat_doctor/features/shifts/presentation/widgets/my_shifts_details_info.dart';
@@ -20,10 +23,28 @@ class _MyShiftsDetailsScreenState extends State<MyShiftsDetailsScreen> {
 
   static const String image = "https://plus.unsplash.com/premium_photo-1672097247893-4f8660247b1f?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=1169";
 
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      bottomNavigationBar: Container(
+        width: double.infinity,
+        padding: EdgeInsets.only(left: 20.w, right: 20.w, top: 15.h, bottom: 30.h),
+        decoration: BoxDecoration(
+          color: Theme.of(context).cardColor,
+          borderRadius: BorderRadius.vertical(top: Radius.circular(25.r)),
+        ),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          spacing: 10.h,
+          children: [
+            AppButton(text: "Withdraw Application",),
+            ClickableText(
+              text: "Cancel Shift",
+              textStyle: TextStyles.textViewSemiBold14.copyWith(color: Theme.of(context).hintColor),
+            )
+          ],
+        ),
+      ),
       appBar: AppAppbar(title: Text("Shift Detials")),
       body: SingleChildScrollView(
         padding: EdgeInsets.only(left: 20.w, right: 20.w, top: 10.h, bottom: 30.h),
